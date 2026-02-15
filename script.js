f0fa094d87bb64aec4c9966ba50ce064716ed43f
Nur Rachmad Arifin <nurrachmadarifin6@gmail.com>
2026-02-16 01:34:42 +0700

    semua perubhan

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 
 hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
 
